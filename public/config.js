83054cab19909c250b15ddd700095da99a1891df
Coby Palivathukal <45637624+cobypali@users.noreply.github.com>
2026-02-16 17:32:25 -0800

    Configure Google Apps Script endpoint

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  appsScriptUrl: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_EXEC_URL_HERE",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzAbBz-UQQ7NVrPSpvNSpZs0I94EgD8NMmm8xRm6tSnid7KQTG4AgKwAw_wO1PPrAwA/exec",
   oneSignalAppId: "PASTE_YOUR_ONESIGNAL_APP_ID_HERE"
 };

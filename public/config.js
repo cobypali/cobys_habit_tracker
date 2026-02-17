b7888733b986eed00ac9c2b2062653542e5e7d89
Coby Palivathukal <45637624+cobypali@users.noreply.github.com>
2026-02-16 18:34:16 -0800

    Update frontend to newest Apps Script deployment URL

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzYV8LB1CJmZZG-HP8UeedccQhuh5fFHFD57qBAuq09JyCHtlSdnx7QAKRGoUj9oc3N/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbw6f5bnRlrfg2txNalh8XTDumk40sFpKhEUXQB634umVH-ucejbbLPy1yoTusd0ftU8/exec",
   oneSignalAppId: "c1717ac9-0df7-462d-9385-041bb9513c9d"
 };

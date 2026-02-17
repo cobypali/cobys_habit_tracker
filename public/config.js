59686b736d40426b3cacf3e895b2eabff44b73ab
Coby Palivathukal <45637624+cobypali@users.noreply.github.com>
2026-02-16 17:55:03 -0800

    Add root index redirect for GitHub Pages and set OneSignal App ID

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   appsScriptUrl: "https://script.google.com/macros/s/AKfycbzAbBz-UQQ7NVrPSpvNSpZs0I94EgD8NMmm8xRm6tSnid7KQTG4AgKwAw_wO1PPrAwA/exec",
-  oneSignalAppId: "PASTE_YOUR_ONESIGNAL_APP_ID_HERE"
+  oneSignalAppId: "c1717ac9-0df7-462d-9385-041bb9513c9d"
 };

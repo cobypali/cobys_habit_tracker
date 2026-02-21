37880c3af93a934d72a33fc9f15908dc3924ff47
Coby Palivathukal <45637624+cobypali@users.noreply.github.com>
2026-02-21 11:05:29 -0800

    Update app to latest Apps Script deployment URL

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxTCGpUIlIypMsM-hc9UjRd1DYWCjSXzuT5L1wCgDDs1eRVSXoU4vD8hn6nIiaF9d2O/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwbndJg5h6rNjaJ56S0euLSrrhds9ZLZhN5zIzhPyEKrLNTuJO75i-Tu54r07wDc0fN/exec",
   oneSignalAppId: "c1717ac9-0df7-462d-9385-041bb9513c9d"
 };
